350c3ea91790deae8e02a761c717af3cd9fd2a43
ughurba <ughurba@edu.code.az>
2022-05-22 14:15:21 +0400

    very nice

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@
 // var result = new CustomMatch(50).plus(6).minus(30).multiply(3).divide(2)
 // resultin neticesi bu yazilisa gore 50+6-30*3/2 mentiqi ile (riyazi prinsibi unudun,vurma bolme onceliyine ehtiyac yoxdu) 39 olmalidi
 
+
 class Calc {
 
     constructor(CustomMatch) {
@@ -38,3 +39,46 @@ console.log(result)
 
 
 // 2) Custom bir array classi yaradin. Hemin arrayin find ve push metodu olsun. Taski ishleyib turn in edin.
+
+class  Arr{
+
+    constructor(array) {
+        this.array = array
+    }
+
+
+        find(id){
+        let result
+
+            for (let i = 0; i < this.array.length; i++) {
+
+                if(this.array[i] === id){
+                    result = this.array[i]
+                    return result;
+
+                }
+
+            }
+
+            return  result
+
+        }
+        i = 0
+        push(el){
+
+            this.array[this.i] = el
+
+            this.i++
+         return this.array
+        }
+}
+var arr = new Arr([])
+arr.push(21);
+arr.push('dsa');
+arr.push(true);
+arr.push(false);
+arr.push({});
+arr.push(33);
+
+console.log(arr)
+
